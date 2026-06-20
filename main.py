7a52afe873c5680264bf455fb48866f4dfdd048d
root <root@localhost.localdomain>
2026-06-20 20:00:31 +0800

    add CORS middleware, increase max_tokens to 8000

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.api import router as api_router
 
@@ -17,6 +18,17 @@ logging.basicConfig(
 )
 
 app = FastAPI(title="AI Backend", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
